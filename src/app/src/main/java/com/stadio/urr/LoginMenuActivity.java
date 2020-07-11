a7340fc6b99ae51d725721d5a8155cd9137a3ae4
Ethan <eitanwass@gmail.com>
2020-07-11 14:04:52 +0300

    Added login, register, and connect menus. client side connection

--- src/app/src/main/java/com/stadio/urr/LoginMenuActivity.java
+++ src/app/src/main/java/com/stadio/urr/LoginMenuActivity.java
@@ -1,15 +1,13 @@
 package com.stadio.urr;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
@@ -19,8 +17,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class RegisterMenuActivity extends AppCompatActivity {
+public class LoginMenuActivity extends AppCompatActivity {
+
+    private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     private Socket mSocket;
     {
@@ -31,12 +33,8 @@ public class RegisterMenuActivity extends AppCompatActivity {
         }
     }
 
-    private TextView repeatPasswordErrorTextView;
-
     private EditText usernameEditText;
-    private EditText emailEditText;
     private EditText passwordEditText;
-    private EditText repeatPasswordEditText;
 
     private ProgressBar progressBar;
     private TextView errorTextView;
@@ -44,67 +42,32 @@ public class RegisterMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_menu);
+        setContentView(R.layout.activity_login_menu);
 
         getReferences();
-
-        repeatPasswordEditText.addTextChangedListener( new RepeatPasswordTextWatched());
 
         ListenForEvents();
     }
 
     private void getReferences() {
-        repeatPasswordErrorTextView = findViewById(R.id.repeatPasswordErrorTextView);
-
         usernameEditText = findViewById(R.id.loginUsernameEditText);
-        emailEditText = findViewById(R.id.registerEmailEditText);
         passwordEditText = findViewById(R.id.loginPasswordEditText);
-        repeatPasswordEditText = findViewById(R.id.registerRepeatPasswordEditText);
 
         progressBar = findViewById(R.id.progressBar);
         errorTextView = findViewById(R.id.errorTextView);
     }
 
-    private class RepeatPasswordTextWatched implements TextWatcher{
-
-        @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable editable) {
-            String password = passwordEditText.getText().toString();
-            String repeatPassword = repeatPasswordEditText.getText().toString();
-
-            final int visibility = password.equals(repeatPassword) ? View.INVISIBLE : View.VISIBLE;
-
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    repeatPasswordErrorTextView.setVisibility(visibility);
-                }
-            });
-        }
-    }
-
     public void ListenForEvents() {
-        mSocket.on("register-success", new Emitter.Listener() {
+        mSocket.on("login-success", new Emitter.Listener() {
             @Override
             public void call(Object... args) {
                 displayMessage(args[0].toString());
 
-                Intent loginIntent = new Intent(getApplicationContext(), LoginMenuActivity.class);
-                startActivity(loginIntent);
+                StartGame();
             }
         });
 
-        mSocket.on("register-failed", new Emitter.Listener() {
+        mSocket.on("login-failed", new Emitter.Listener() {
             @Override
             public void call(Object... args) {
                 displayMessage(args[0].toString());
@@ -112,27 +75,25 @@ public class RegisterMenuActivity extends AppCompatActivity {
         });
     }
 
-    public void registerOnClick(View view) {
-        sendRegisterData(mSocket);
-
+    public void loginOnClick(View view) {
         progressBar.setVisibility(View.VISIBLE);
+
+        sendLoginData(mSocket);
     }
 
-    private void sendRegisterData(Socket socket){
+    private void sendLoginData(Socket socket) {
         String username = usernameEditText.getText().toString();
-        String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
-        // TODO: Hash password.
+        // TODO: hash password.
 
         JSONObject emissionJson = new JSONObject();
         try {
             emissionJson.put("username", username);
-            emissionJson.put("email", email);
             emissionJson.put("password", password);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        socket.emit("register", emissionJson);
+        socket.emit("login", emissionJson);
     }
 
     private void displayMessage(final String displayMessage) {
@@ -143,5 +104,14 @@ public class RegisterMenuActivity extends AppCompatActivity {
                 errorTextView.setText(displayMessage);
             }
         });
+    }
+
+    private void StartGame() {
+        LOGGER.log(Level.INFO, "Start game in online mode.");
+    }
+
+    public void switchToRegisterMenu(View view) {
+        Intent registerIntent = new Intent(getApplicationContext(), RegisterMenuActivity.class);
+        startActivity(registerIntent);
     }
 }
